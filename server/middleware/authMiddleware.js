3327009127203c2318c15303fbbea9ee1b9f713d
mohaRufaci <meionezzspecial@gmail.com>
2026-04-12 17:50:43 +0300

    Initialize project on new GitHub repository

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
+const { prisma } = require('../config/db');
 
 const protect = async (req, res, next) => {
   let token;
@@ -13,7 +13,15 @@ const protect = async (req, res, next) => {
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = await User.findById(decoded.id).select('-password');
+      req.user = await prisma.user.findUnique({
+        where: { id: decoded.id },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          role: true,
+        },
+      });
 
       next();
     } catch (error) {
